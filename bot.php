8aa215908ba94bf7a11f12e04e311c2e53a61ebe
Nancy <a.khorevich@gmail.com>
2016-04-28 07:39:59 +0300

    add new

--- bot.php
+++ bot.php
@@ -1,22 +1,50 @@
 <?php
-
+/**
+ * Telegram Bot access token и URL.
+ */
 $access_token = '205627578:AAEWQRBVx7lHl3O1z5tmoUqDxyM4IBI0ANo';
 $api = 'https://api.telegram.org/bot' . $access_token;
 
+/**
+ * Задаём основные переменные.
+ */
 $output = json_decode(file_get_contents('php://input'), TRUE);
 $chat_id = $output['message']['chat']['id'];
 $first_name = $output['message']['chat']['first_name'];
 $message = $output['message']['text'];
 
-switch ($message) {
-	case '/vk':
-		$mes = 'https://vk.com/svideteli_koroleva';
-		sendMessage($chat_id,$mes);
-		break;
-	
-	default:
-		# code...
-		break;
+/**
+ * Emoji для лучшего визуального оформления.
+ */
+$emoji = array(
+  'preload' => json_decode('"\uD83D\uDE03"'), // Улыбочка.
+  'weather' => array(
+    'clear' => json_decode('"\u2600"'), // Солнце.
+    'clouds' => json_decode('"\u2601"'), // Облака.
+    'rain' => json_decode('"\u2614"'), // Дождь.
+    'snow' => json_decode('"\u2744"'), // Снег.
+  ),
+);
+
+/**
+ * Получаем команды от пользователя.
+ */
+switch($message) {
+  // API погоды предоставлено OpenWeatherMap.
+  // @see http://openweathermap.org
+  case '/vk':
+    // Отправляем приветственный текст.
+    $preload_text = 'Привет, ' . $first_name . '! ' . $emoji['preload'];
+    sendMessage($chat_id, $preload_text);
+
+    break;
+  default:
+    break;
 }
 
-?>
+/**
+ * Функция отправки сообщения sendMessage().
+ */
+function sendMessage($chat_id, $message) {
+  file_get_contents($GLOBALS['api'] . '/sendMessage?chat_id=' . $chat_id . '&text=' . urlencode($message));
+}
